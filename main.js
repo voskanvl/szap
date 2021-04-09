56fdd28d344733d3ddcccc3d353c71f1b891133d
Anton Voskanyan <voskanvl@gmail.com>
2021-04-09 11:49:16 +0300

    9.04.21 11:49

--- main.js
+++ main.js
@@ -2,23 +2,55 @@
 
 let dragEl = document.querySelector(".specification__drag");
 let panel = document.querySelector(".panel.specification");
-const { width: panelWidth } = panel.getBoundingClientRect();
+let { width: panelWidth } = panel.getBoundingClientRect();
 document.ondragstart = function () {
     return false;
 };
-dragEl.onmousedown = e => {
-    const moveAt = ev => {
-        const newWidth = ev.pageX - dragEl.offsetWidth / 2;
-        if (newWidth > panelWidth && newWidth < panelWidth * 2)
-            panel.style.width = newWidth + "px";
-        // dragEl.style.top = ev.pageY - dragEl.offsetHeight / 2 + "px";
-    };
-    document.onmousemove = ev => {
-        moveAt(ev);
-    };
-    document.onmouseup = () => {
-        document.onmousemove = null;
-        dragEl.onmouseup = null;
-    };
-    moveAt(e);
+
+// dragEl.onmousedown = e => {
+//     const moveAt = ev => {
+//         const newWidth = ev.pageX - dragEl.offsetWidth / 2;
+//         if (newWidth > panelWidth && newWidth < panelWidth * 2)
+//             panel.style.width = newWidth + 8 + "px";
+//         // dragEl.style.top = ev.pageY - dragEl.offsetHeight / 2 + "px";
+//     };
+//     document.onmousemove = ev => {
+//         moveAt(ev);
+//     };
+//     document.onmouseup = () => {
+//         document.onmousemove = null;
+//         dragEl.onmouseup = null;
+//     };
+//     moveAt(e);
+// };
+const moveAt = ev => {
+    const newWidth = ev.pageX - dragEl.offsetWidth / 2;
+    if (newWidth > panelWidth && newWidth < panelWidth * 2)
+        panel.style.width = newWidth + 8 + "px";
+    // dragEl.style.top = ev.pageY - dragEl.offsetHeight / 2 + "px";
 };
+let downed = false;
+let panelEnd = false;
+
+dragEl.addEventListener("mousedown", () => {
+    downed = true;
+    console.log(downed);
+});
+document.addEventListener("mouseup", () => {
+    downed = false;
+    console.log(downed);
+});
+document.addEventListener("mousemove", e => {
+    if (downed) {
+        moveAt(e);
+        console.log(e);
+    }
+});
+dragEl.addEventListener("dblclick", () => {
+    console.log("doubleclick");
+    panelEnd
+        ? (panel.style.width = panelWidth + "px")
+        : (panel.style.width = panelWidth * 2 + "px");
+    panelEnd = !panelEnd;
+});
+onresize = () => (panelWidth = panel.getBoundingClientRect().width);
