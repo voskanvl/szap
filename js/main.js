6194359a397d063df7de85277597f808e95b7fff
Anton Voskanyan <voskanvl@gmail.com>
2021-04-09 18:10:25 +0300

    9.04.21 18:10

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@
 import "./shorts";
 import "./init";
 import "./dragEl.js";
-import selectPointer from "./selectPointer";
+
+import { selectPointer } from "./selectPointer";
 import data from "../data.json";
 
 let currentIndex = 0;
+// [0, 1, 2, 3].forEach(e => {
+//     setTimeout(() => selectPointer(e), 2000);
+// });
+selectPointer(currentIndex);
+window.selectPointer = selectPointer;
--- js/selectPointer.js
+++ js/selectPointer.js
@@ -1,9 +1,40 @@
 /** @format */
-
+import { $, $$, _ } from "./shorts.js";
 import "./shorts";
 
 // const pointer = $(".select-carousel__pointer");
-// const items = $$(".select-carousel__item");
+const pointer = $(".pointer");
+const items = $$(".select-carousel__item");
+const selectCarouselContainer = $(".select-carousel__container");
+const selectCarousel = $(".select-carousel");
+// const selectCarouselLeft = selectCarousel.getBoundingClientRect().left;
 
-const selectPointer = current => {};
-export default selectPointer;
+export const calculateItemCenter = () =>
+    items.forEach((e, i) => {
+        const { left, width } = e.getBoundingClientRect();
+        // const left = e.offsetLeft - selectCarouselContainer.scrollleft;
+        // const width = e.offsetWidth;
+        const center = left + width / 2;
+        _(i, center);
+        items[i].center = center;
+    });
+
+calculateItemCenter();
+
+let currentEl = 0;
+
+export const selectPointer = current => {
+    currentEl = current;
+    const left = items[current].center;
+    _(left, pointer);
+    pointer.style.left = left + "px";
+};
+
+const recalc = () =>
+    setTimeout(() => {
+        calculateItemCenter();
+        selectPointer(currentEl);
+    });
+
+selectCarouselContainer.addEventListener("scroll", recalc);
+window.addEventListener("resize", recalc);
