a8a2c3f936b9c947c6cfdeab13f276f908ae74c6
Anton Voskanyan <voskanvl@gmail.com>
2021-04-12 22:07:08 +0300

    работает нижний флажок

--- js/withState/pointers/selectPointer.js
+++ js/withState/pointers/selectPointer.js
@@ -1,7 +1,9 @@
 import { state } from "../../state";
 import { $, $$, _ } from "../../shorts.js";
 import { gsap } from "gsap";
+import { limiter } from "../../limiter.js";
 
+const container = $(".select-carousel__container");
 const selectItems = $$(".select-carousel__item");
 
 const center = element => {
@@ -9,9 +11,35 @@ const center = element => {
     return left + width / 2;
 };
 
-state.on("index", x => {
+const render = x => {
+    const {
+        left: containerLeft,
+        width: containerWidth,
+    } = container.getBoundingClientRect();
+
+    const pointer = limiter(x)(containerLeft, containerLeft + containerWidth);
+
     gsap.to(".select-pointer", {
-        x: center(selectItems[x]),
+        x: pointer.value,
+        rotate: () => {
+            if (pointer.less) return -90;
+            if (pointer.more) return 90;
+            return 0;
+        },
         ease: "elastic.out(1,0.3)",
     });
-});
+};
+
+state.on("index", x => render(center(selectItems[x])));
+
+container.addEventListener("scroll", () =>
+    render(center(selectItems[state.getState().index])),
+);
+container.addEventListener("mousewheel", () =>
+    render(center(selectItems[state.getState().index])),
+);
+window.addEventListener("resize", () =>
+    render(center(selectItems[state.getState().index])),
+);
+
+render(center(selectItems[state.getState().index]));
