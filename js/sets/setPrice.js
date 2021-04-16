9be5669237108c59959c0a205ace5686a5d8e1c7
Anton Voskanyan <voskanvl@gmail.com>
2021-04-16 11:33:36 +0300

    remove set folder

--- js/sets/setPrice.js
+++
@@ -1,7 +0,0 @@
-import { $, _ } from "../shorts.js";
-import { gsap } from "gsap";
-import { data } from "../data.js";
-export const setPrice = x => {
-    $(".description__price-value").textContent = data[x].price;
-    gsap.from(".description__price-value", { opacity: 0, scale: 0.2 });
-};
